88172dc10a0918a3293c8b493080d1059856300d
OwOHamper <barca.teo@gmail.com>
2021-11-12 18:44:59 +0100

    deleted useless files

--- src/table.py
+++
@@ -1,8 +0,0 @@
-from prettytable import PrettyTable
-
-
-class Table:
-    def __init__(self):
-        pass
-    def add_row_table(self, table: PrettyTable, args: list):
-        table.add_rows([args])
