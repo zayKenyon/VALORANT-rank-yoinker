6170fa616b294faa6ddb26ae8bba678ccefc73d0
D3crypt360 <nahilali3600@gmail.com>
2022-01-19 02:03:23 +0500

    fixed how config is generated

--- src/config.py
+++ src/config.py
@@ -2,12 +2,20 @@ import json
 from io import TextIOWrapper
 from json import JSONDecodeError
 import requests
+import os
 
 from src.logs import Logging
 
 class Config:
     def __init__(self, log):
         self.log = log
+
+        if not os.path.exists("config.json"):
+            self.log("config.json not found, creating new one")
+            with open("config.json", "w") as file:
+                config = self.config_dialog(file)
+            
+
         try:
             with open("config.json", "r") as file:
                 self.log("config opened")
@@ -17,25 +25,31 @@ class Config:
                     config = self.config_dialog(file)
 
                 if config["weapon"] == "":
+                    weapon = input("Enter the name of the weapon you use the most (This is for tracking the skins): ").capitalize().strip()
+                    self.log(f"User inputted {weapon} as the weapon")
                     with open("config.json", "w") as f:
-                        weapon = input("Enter the name of the weapon you use the most (This is for tracking the skins): ").capitalize().strip()
-                        self.log(f"User inputted {weapon} as the weapon")
-                        if weapon not in [weapon["displayName"] for weapon in requests.get("https://valorant-api.com/v1/weapons").json()["data"]]:
+                        if not self.weapon_check(weapon):
                             config["weapon"] = "vandal"
                             json.dump(config, f, indent=4)
-                            self.log(f"{weapon} weapon has been added to the config file by default")
+                            self.log("vandal weapon has been added to the config file by default")
                         else:
                             config["weapon"] = weapon
                             json.dump(config, f, indent=4)
                             self.log(f"{weapon} weapon has been added to the config file by user")
                 
-        except (FileNotFoundError, JSONDecodeError):
-            self.log("file not found or invalid file")
+        except (JSONDecodeError):
+            self.log("invalid file")
             with open("config.json", "w") as file:
                 config = self.config_dialog(file)
         finally:
             self.cooldown = config["cooldown"]
             self.log(f"got cooldown with value '{self.cooldown}'")
+
+            if not self.weapon_check(config["weapon"]):
+                    self.weapon = "vandal" # if the user manually entered a wrong name into the config file, this will be the default until changed by the user.
+            else:   
+                self.weapon = config["weapon"]
+                
 
     def config_dialog(self, fileToWrite: TextIOWrapper):
         self.log("color config prompt called")
@@ -43,3 +57,9 @@ class Config:
         
         json.dump(jsonToWrite, fileToWrite)
         return jsonToWrite
+
+    def weapon_check(self, name):
+        if name in [weapon["displayName"] for weapon in requests.get("https://valorant-api.com/v1/weapons").json()["data"]]:
+            return True
+        else:
+            return False
