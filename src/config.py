49e2719c90b4916bb75f648de8a8aa885b55b8bd
D3crypt360 <nahilali3600@gmail.com>
2022-05-25 18:28:55 +0500

    config file has now proper fallbacks

--- src/config.py
+++ src/config.py
@@ -9,17 +9,40 @@ from src.logs import Logging
 class Config:
     def __init__(self, log):
         self.log = log
+        self.keys = ["cooldown", "weapon", "port"]
 
         if not os.path.exists("config.json"):
             self.log("config.json not found, creating new one")
             with open("config.json", "w") as file:
                 config = self.config_dialog(file)
             
-
         try:
+
             with open("config.json", "r") as file:
                 self.log("config opened")
                 config = json.load(file)
+                keys = [k for k in config.keys()]
+
+            if len(self.keys) != len(keys):
+                with open("config.json", 'w') as w:
+                    if self.keys[0] not in keys and self.keys[1] not in keys and self.keys[2] not in keys:
+                        config = self.config_dialog(file)
+                
+                    if self.keys[0] not in keys:
+                        config.update({self.keys[0]: 1})
+                        self.log("cooldown has been added to the config file by default")
+
+                    if self.keys[1] not in keys:
+                        config.update({self.keys[1]: ''})
+                        self.log("weapon has been added to the config file by default")
+                    
+                    if self.keys[2] not in keys:
+                        config.update({self.keys[2]: 1100})
+                        self.log("port has been added to the config file by default")
+
+                    json.dump(config, w, indent=4)
+
+
                 if config.get("cooldown") is None:
                     self.log("some config values are None, getting new config")
                     config = self.config_dialog(file)
@@ -54,7 +77,7 @@ class Config:
 
     def config_dialog(self, fileToWrite: TextIOWrapper):
         self.log("color config prompt called")
-        jsonToWrite = {"cooldown": 1, "weapon":"", "port":1100}
+        jsonToWrite = {self.keys[0]: 1, self.keys[1]: "", self.keys[2]: 1100}
         
         json.dump(jsonToWrite, fileToWrite)
         return jsonToWrite
