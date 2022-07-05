60c900db1c73ea4ded1095b72705730cad8b7571
OwOHamper <barca.teo@gmail.com>
2022-07-05 13:39:54 +0200

    saving stats

---
+++ src/stats.py
@@ -0,0 +1,33 @@
+import os
+import time
+import json
+
+class Stats:
+    def __init__(self):
+        pass
+
+    def save_data(self, data):
+        # path = os.path.join(os.getenv('APPDATA'), R'vry\stats.json')
+        try:
+            os.mkdir(os.path.join(os.getenv('APPDATA'), "vry"))
+        except FileExistsError:
+            pass
+        try:
+            with open(os.path.join(os.getenv('APPDATA'), "vry/stats.json"), "r") as f:
+                original_data = json.load(f)
+        except (FileNotFoundError, json.decoder.JSONDecodeError):
+            original_data = {}
+
+        updated_data = original_data
+        for puuid in data.keys():
+            if original_data.get(puuid) is None:
+                updated_data.update({puuid: [data[puuid]]})
+            else:
+                updated_data[puuid].append(data[puuid])
+        
+        # updated_data.update(data)
+        # print(updated_data)
+
+        with open(os.path.join(os.getenv('APPDATA'), "vry/stats.json"), "w") as f:
+            json.dump(updated_data, f)
+    
