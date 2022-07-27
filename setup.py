2b9fee3ac7cf1f686a13300eabaf6077f296aa64
tanishqmanuja <tanishqmanuja@gmail.com>
2022-07-27 15:37:37 +0530

    chore(setup.py): deprecated targetName

--- setup.py
+++ setup.py
@@ -12,6 +12,6 @@ setup(
     name = "VALORANT rank yoinker",
     version = version,
     description='vRY - Live Match Rank Viewer',
-    executables = [Executable("main.py", icon="./assets/Logo.ico", targetName="vry.exe")],
+    executables = [Executable("main.py", icon="./assets/Logo.ico", target_name="vry.exe")],
     options={"build_exe": build_exe_options}
 )
