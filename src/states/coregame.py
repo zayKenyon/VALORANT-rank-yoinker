87e55396077f3e0a7647cd9163effbad900c0cf5
OwOHamper <barca.teo@gmail.com>
2022-08-24 11:18:28 +0200

    if match id is 0 then fetch again after 5 seconds

--- src/states/coregame.py
+++ src/states/coregame.py
@@ -1,4 +1,4 @@
-
+import time
 
 
 
@@ -18,7 +18,16 @@ class Coregame:
             return match_id
         except (KeyError, TypeError):
             self.log(f"cannot find coregame match id: ")
-            print(f"No match id found. {self.response}")
+            # print(f"No match id found. {self.response}")
+            time.sleep(5)
+            try:
+                self.response = self.Requests.fetch(url_type="glz", endpoint=f"/core-game/v1/players/{self.Requests.puuid}", method="get")
+                match_id = self.response['MatchID']
+                self.log(f"retrieved coregame match id: '{match_id}'")
+                return match_id
+            except (KeyError, TypeError):
+                self.log(f"cannot find coregame match id: ")
+                print(f"No match id found. {self.response}")
             return 0
 
     def get_coregame_stats(self):
