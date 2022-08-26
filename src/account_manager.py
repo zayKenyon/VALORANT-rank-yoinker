38729d7e7ec4e6afb4dc38594420a3dba7b3c127
OwOHamper <barca.teo@gmail.com>
2022-08-26 22:40:01 +0200

    account manager file

---
+++ src/account_manager.py
@@ -0,0 +1,16 @@
+
+
+
+class AccountManager:
+    def __init__(self, log):
+        self.log = log
+
+    def get_current_account(self):
+        pass
+
+    def switch_to_account(self, account):
+        pass
+
+
+if __name__ == "__main__":
+    pass
