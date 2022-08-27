319ae91a64a4819b656e5697fa02f9f0dd4c9f87
OwOHamper <barca.teo@gmail.com>
2022-08-27 12:34:42 +0200

    typo

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from src.constants import version
 build_exe_options = {
     "path": sys.path,
     "include_files":['configurator.bat', 'updatescript.bat'],
-    "packages": ["requests", "colr", "InquirerPy", "websoc,kets", "pypresence", "nest_asyncio", "rich", "websocket_server"],
+    "packages": ["requests", "colr", "InquirerPy", "websockets", "pypresence", "nest_asyncio", "rich", "websocket_server"],
     "excludes": ["tkinter", "test", "unittest", "pygments"]
 }
 
