f826def76271f6914f1fcc15c660849553127878
zay <isaac@kenyon.co.uk>
2022-08-28 02:59:13 +0100

    style(logs): PEP8

--- src/logs.py
+++ src/logs.py
@@ -1,28 +1,40 @@
-import glob
+"""Record VALORANT-rank-yoinker's actions and state."""
+import locale
+from glob import glob
 import os
 import time
 
-class Logging:
-    def __init__(self):
-        self.logFileOpened = False
 
-    def log(self, stringToLog: str):
-        # creating logs folder
+# pylint: disable=too-few-public-methods
+class Logging:
+    """Handles logging vRY's actions and state to the logs' folder."""
+
+    def __init__(self):
+        self.log_file_opened = False
+
+    def log(self, string_to_log: str):
+        """Creates a folder for and logs the actions and state."""
         try:
-            os.mkdir(os.getcwd() + "\logs")
+            os.mkdir(os.getcwd() + r"\logs")
         except FileExistsError:
             pass
         filenames = []
-        for filename in glob.glob(r"logs/log-*.txt"):
+        for filename in glob(r"logs/log-*.txt"):
             filenames.append(int(filename[9:-4]))
         if len(filenames) == 0:
             filenames.append(0)
-        if self.logFileOpened:
-            with open(f"logs/log-{max(filenames)}.txt", "a") as logFile:
-                logFile.write(f"[{time.strftime('%Y.%m.%d-%H.%M.%S', time.localtime(time.time()))}]"
-                              f" {stringToLog.encode('ascii', 'replace').decode()}\n")
+        if self.log_file_opened:
+            with open(f"logs/log-{max(filenames)}.txt",
+                      "a", encoding=locale.getpreferredencoding()) as log_file:
+                log_file.write(
+                    f"""[{time.strftime('%Y.%m.%d-%H.%M.%S',
+                                        time.localtime(time.time()))}]"""
+                    f" {string_to_log.encode('ascii', 'replace').decode()}\n")
         else:
-            with open(f"logs/log-{max(filenames) + 1}.txt", "w") as logFile:
-                self.logFileOpened = True
-                logFile.write(f"[{time.strftime('%Y.%m.%d-%H.%M.%S', time.localtime(time.time()))}]"
-                              f" {stringToLog.encode('ascii', 'replace').decode()}\n")
+            with open(f"logs/log-{max(filenames) + 1}.txt",
+                      "w", encoding=locale.getpreferredencoding()) as log_file:
+                self.log_file_opened = True
+                log_file.write(
+                    f"""[{time.strftime('%Y.%m.%d-%H.%M.%S',
+                                        time.localtime(time.time()))}]"""
+                    f" {string_to_log.encode('ascii', 'replace').decode()}\n")
