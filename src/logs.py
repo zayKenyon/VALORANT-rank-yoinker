95a7573461030f7b96171296484e6361b97029f8
nanometer5088 <code.deleo@simplelogin.com>
2023-02-12 17:51:37 -0300

    Rewritten and commented src/logs.py

--- src/logs.py
+++ src/logs.py
@@ -1,28 +1,42 @@
+# This code imports the `glob`, `os`, and `time` modules
 import glob
 import os
 import time
 
+# The `Logging` class is defined
 class Logging:
+    # The class constructor initializes the `logFileOpened` attribute as `False`
     def __init__(self):
         self.logFileOpened = False
 
-    def log(self, stringToLog: str):
-        # creating logs folder
-        try:
-            os.mkdir(os.getcwd() + "\logs")
-        except FileExistsError:
-            pass
-        filenames = []
-        for filename in glob.glob(r"logs/log-*.txt"):
-            filenames.append(int(filename[9:-4]))
-        if len(filenames) == 0:
-            filenames.append(0)
-        if self.logFileOpened:
-            with open(f"logs/log-{max(filenames)}.txt", "a") as logFile:
-                logFile.write(f"[{time.strftime('%Y.%m.%d-%H.%M.%S', time.localtime(time.time()))}]"
-                              f" {stringToLog.encode('ascii', 'replace').decode()}\n")
-        else:
-            with open(f"logs/log-{max(filenames) + 1}.txt", "w") as logFile:
-                self.logFileOpened = True
-                logFile.write(f"[{time.strftime('%Y.%m.%d-%H.%M.%S', time.localtime(time.time()))}]"
-                              f" {stringToLog.encode('ascii', 'replace').decode()}\n")
+    # The `log` method takes a string `log_string` as an argument
+    def log(self, log_string: str):
+        # The logs directory is determined
+        logs_directory = os.getcwd() + "/logs"
+
+        # If the logs directory does not exist, it is created
+        if not os.path.exists(logs_directory):
+            os.mkdir(logs_directory)
+        
+        # A list of log files is obtained using the `glob` module
+        log_files = glob.glob(r"logs/log-*.txt")
+
+        # The log file numbers are extracted from the filenames
+        log_file_numbers = [int(file[9:-4]) for file in log_files]
+        
+        # If no log files exist, a log-0.txt file is created
+        if not log_file_numbers:
+            log_file_numbers.append(0)
+        
+        # The log file name is determined
+        log_file_name = f"logs/log-{max(log_file_numbers) + 1 if not self.logFileOpened else max(log_file_numbers)}.txt"
+
+        # The log file is opened and written to
+        with open(log_file_name, "a" if self.logFileOpened else "w") as log_file:
+            self.logFileOpened = True
+
+            # The current time is formatted
+            current_time = time.strftime("%Y.%m.%d-%H.%M.%S", time.localtime(time.time()))
+
+            # The log string is written to the file with the current time as a prefix
+            log_file.write(f"[{current_time}] {log_string.encode('ascii', 'replace').decode()}\n")
