043bf9d9131493584b3183139f1bdcd6bf27bab0
Ayydxn <arcanewastakenofficial@gmail.com>
2024-03-17 21:45:46 -0400

    Added comment to explain the need for admin permissions
    - Added a comment to explain the need for admin permissions in the install.bat script.

--- install.bat
+++ install.bat
@@ -1,5 +1,7 @@
 @echo off
 
+@REM Admin privileges are needed because without them, the installer won't be able to place a shortcut to vRY in the Start Menu folder, therefore not allowing you to access vRY as easily.
+@REM By "Start menu folder", I'm referring to C:\ProgramData\Microsoft\Windows\Start Menu\Programs
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 
 cd %~dp0
