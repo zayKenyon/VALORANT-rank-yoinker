f0adc21e059ab92f2b156329b3de6ff406bcec0d
PINTOSO <otacopintoso@gmail.com>
2025-01-09 15:38:24 -0300

    Py to Python command

--- configurator.bat
+++ configurator.bat
@@ -3,7 +3,7 @@
 if %ERRORLEVEL% NEQ 0 (
     color 0C
     echo Executable failed, trying Python...
-    py main.py --config 2>nul
+    python main.py --config 2>nul
     if %ERRORLEVEL% NEQ 0 (
         echo Python also failed!
         echo Try reinstalling VRY or Python to fix this issue.
